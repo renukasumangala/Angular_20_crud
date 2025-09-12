7b8623e701135372debe1574ad83a8eb20daa3eb
Renuka <renukasumangala.b@gmail.com>
2025-09-12 11:34:25 +0530

    Add files via upload
    Updated  Angular Project

--- associate.ts
+++ associate.ts
@@ -1,8 +1,36 @@
-export interface associateModel{
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { associateModel } from '../../model/associate';
 
-    id: number,
-    name: string,
-    address: string,
-    creditlimit: number,
-    status: boolean
+@Injectable({
+  providedIn: 'root'
+})
+export class Associate {
+  apiBaseUrl='http://localhost:3000';
+
+
+  constructor(private http: HttpClient){
+
   }
+
+  Getall() {
+   return this.http.get<associateModel[]>(this.apiBaseUrl + '/associate');
+  }
+
+  Get(id: number) {
+    return this.http.get<associateModel>(this.apiBaseUrl + '/associate/' +id);
+  }
+
+  Delete(id: number) {
+   return this.http.delete(this.apiBaseUrl + '/associate/'+ id);
+  }
+
+  Update(data: associateModel) {
+    return this.http.put(this.apiBaseUrl + '/associate/' + data.id, data);
+  }
+
+  Create(data: associateModel) {
+   return this.http.post(this.apiBaseUrl + '/associate', data);
+  }
+
+}
--- list.ts
+++ list.ts
@@ -1,8 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
-import { MatTableModule } from '@angular/material/table'
+import { MatTableDataSource, MatTableModule } from '@angular/material/table'
 import { MatDialogModule } from '@angular/material/dialog'
+import { Associate } from '../../_shared/associate';
+import { associateModel } from '../../../model/associate';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -11,13 +14,30 @@ import { MatDialogModule } from '@angular/material/dialog'
   styleUrl: './list.css'
 })
 export class List implements OnInit,OnDestroy{
- constructor(){
-  
+
+  _list:associateModel[] = [];
+  subs=new Subscription();
+  displayHeaders=['id','name','address','cl','status','action']
+  datasource!: MatTableDataSource<associateModel>;
+ constructor(private service:Associate) {
+
  }
+
+  GetallList() {
+    let _sub= this.service.Getall().subscribe(item => {
+     this._list = item;
+     this.datasource= new MatTableDataSource(this._list);
+
+    });
+    this.subs.add(_sub);
+  }
+
   ngOnInit(): void {
+    this.GetallList();
+    
       
   }
   ngOnDestroy(): void {
-      
+      this.subs.unsubscribe();
   }
 }
